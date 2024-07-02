e83cc71c756303516b2d338afe3963355f0acdf9
Daniel Gavidia <dgavidia1@gmail.com>
2024-07-02 10:58:09 -0400

    resolved result dashboard output not fitting

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,38 +1,65 @@
 import { InterfaceResult } from "../interfaces/interfaces";
 
+const getFontSize = (number: number): object => {
+    const startingFontSize = 50;
+    const decayFactor = 0.07;
+    const defaultMaxDigits = 10;
+    if (number === 0) {
+        return {
+            fontSize: `${startingFontSize}px`,
+        };
+    } else {
+        const str = number.toString();
+        const digits = str.length;
+        if (digits <= defaultMaxDigits) {
+            return {
+                fontSize: `${startingFontSize}px`,
+            };
+        } else if (digits > defaultMaxDigits) {
+            const iterations = digits - defaultMaxDigits;
+            const dynamicFontSize = Math.floor(
+                startingFontSize * Math.pow(1 - decayFactor, iterations)
+            );
+            return {
+                fontSize: `${dynamicFontSize}px`,
+            };
+        }
+    }
+    return {
+        fontSize: `${startingFontSize}px`,
+    };
+};
+
 const Result: React.FC<InterfaceResult> = ({
     input,
     output,
     inputMode,
     inputCount,
-    decimalBoolean,
-    decimalMagnitude,
 }) => {
     if (inputMode) {
-        if (inputCount === 0 && decimalBoolean === false) {
-            return <div className="result">{input}</div>;
-        } else if (inputCount !== 0 && decimalBoolean === false) {
-            const inputFormatted = input.toPrecision(inputCount);
-            return <div className="result">{inputFormatted}</div>;
-        } else if (
-            inputCount !== 0 &&
-            decimalBoolean === true &&
-            decimalMagnitude === 0.1
-        ) {
-            const inputFormatted = input.toPrecision(inputCount);
-            return <div className="result">{inputFormatted}.</div>;
-        } else if (
-            inputCount !== 0 &&
-            decimalBoolean === true &&
-            decimalMagnitude !== 0.1
-        ) {
-            const inputFormatted = input.toPrecision(inputCount);
-            return <div className="result">{inputFormatted}</div>;
+        if (inputCount === 0) {
+            return (
+                <div className="result" style={getFontSize(input)}>
+                    {input}
+                </div>
+            );
         } else {
-            return <div className="result">{input}</div>;
+            const inputFormatted =
+                inputCount > 21
+                    ? input.toString()
+                    : input.toPrecision(inputCount);
+            return (
+                <div className="result" style={getFontSize(Number(input))}>
+                    {inputFormatted}
+                </div>
+            );
         }
     } else if (!inputMode) {
-        return <div className="result">{output}</div>;
+        return (
+            <div className="result" style={getFontSize(output)}>
+                {output}
+            </div>
+        );
     }
 };
 
